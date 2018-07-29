c82dc99a1573cc1fbb6457862bf5f5e448a1ab9c
Sebben2 <sebastianrundberg@hotmail.com>
2018-07-29 16:20:43 +0200

    can add and remove days, pension date visible

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,11 +8,19 @@ import { Component } from "@angular/core";
 export class AppComponent {
   detractOneDay() {
     if(this.daysLeft>0) {this.daysLeft--;}  // don't go negative
+    this.updateComb();
+  }
+  addOneDay() {
+    this.daysLeft++;
+    this.updateComb();
+  }
+  updateComb() {
     const widthIncrement = 7.833333333333;  
     this.fullCombWidth = Math.max(60 + Math.floor(this.daysLeft*widthIncrement),0); // cap min width at 0
     console.log('daysLeft är '+this.daysLeft+', satt width till '+this.fullCombWidth);
   }
   daysLeft = 66;
+  pensionDate = (new Date()).setDate((new Date()).getDate() + this.daysLeft);
   fullCombWidth = 577;
   constructor() {
   }
