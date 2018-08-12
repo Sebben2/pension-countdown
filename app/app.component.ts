9f4e6606d5344a35fd70bb59c730ad41b7a08f7a
Sebben2 <sebastianrundberg@hotmail.com>
2018-08-12 13:41:45 +0200

    release build för pappsen

--- app/app.component.ts
+++ app/app.component.ts
@@ -32,23 +32,31 @@ export class AppComponent {
   editButtonStyle() {
     return (this.dateModificationVisibility == "hidden" ? "btn btn-secondary" : "btn btn-primary");
   }
+  hideStatusBar() {
+    let window = application.android.startActivity.getWindow();
+    let decorView = window.getDecorView();
+    decorView.setSystemUiVisibility(
+      android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+      | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+      | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+    );
+  }
   pensionDate = new Date(appSettings.getNumber('pensionDate',(new Date(2018,9,3)).valueOf()));
   daysLeft = this.updateDaysLeft();
   fullCombWidth = this.updateCombWidth();
   dateModificationVisibility = "hidden";
   constructor() {
     // comb must be updated to reflect the amount of days left
+    this.updateComb();
     setInterval(() => this.updateComb(), 60000);
+    // never turn off screen
     keepAwake().then(function() {
       console.log("Insomnia is active");
     })
-    let window = application.android.startActivity.getWindow();
-    let decorView = window.getDecorView();
-    decorView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-      | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-      | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-      | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-      | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-      | android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    // rolling with android 4.3, so have to hack layout when lacking immersive flags
+    this.hideStatusBar();
+    setInterval(() => this.hideStatusBar(), 10000);
   }
 }
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
-// import { CombModule } from "./comb.module";
+import { enableProdMode } from '@angular/core';
 
+enableProdMode();
 platformNativeScriptDynamic().bootstrapModule(AppModule);
-// platformNativeScriptDynamic().bootstrapModule(CombModule);
